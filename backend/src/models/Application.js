1b541a63c2acae6ebe0dfe527e1c01d60a898a72
nadav5199 <nadavpassal@gmail.com>
2026-03-15 14:30:24 +0200

    created the schema structure

---
+++ backend/src/models/Application.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const applicationSchema = new mongoose.Schema({
+    companyName: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    description: {
+        type: String,
+        required: true
+    },
+
+});
+module.exports = mongoose.model('Application', applicationSchema);
