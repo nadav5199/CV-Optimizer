1484b0d2a5b7fdd83c8bf66e22029ac002202498
nadav5199 <nadavpassal@gmail.com>
2026-03-15 14:55:32 +0200

    Add Express dependency and set up basic server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+const Application = require('./models/Application');
+const express = require('express')
 const uri = 'mongodb://localhost:27017/CV-Optimizer';
 
 async function connect() {
@@ -12,3 +13,10 @@ async function connect() {
 }
 
 connect();
+const app = express()
+
+app.listen(8080, () => {
+    console.log('Server running on port 8080')
+})
+
+
